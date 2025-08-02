69ee1092a23c624749949b639162a8219eb861ab
alfianchii <pianaee379@gmail.com>
2025-08-02 21:39:18 +0700

    chore: upgrade deps

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/alfianchii/golang-module-say-hello-app
 
 go 1.23.3
 
-require github.com/alfianchii/golang-module-say-hello v1.5.0
+require github.com/alfianchii/golang-module-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	golang_module_say_hello "github.com/alfianchii/golang-module-say-hello"
+	golang_module_say_hello "github.com/alfianchii/golang-module-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(golang_module_say_hello.SayHello())
+	fmt.Println(golang_module_say_hello.SayHello("タカ"))
 }
